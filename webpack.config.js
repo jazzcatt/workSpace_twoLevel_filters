1d40e2ca46f0f1bfadcbb3a894b2bb7a748fa937
jazzcatt <jazzcatt07@gmail.com>
2016-11-01 11:09:28 +0200

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './src/js/main.js',
+	watch: true,
+	output: {
+		path: __dirname+'/public/dist', 
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false,
+				drop_console: true,
+				unsafe: true
+			}
+		})
+	],
+	devtool: 'source-map',
+	module: {
+		loaders: [
+			{
+				test: /.js?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	},
+
+};
