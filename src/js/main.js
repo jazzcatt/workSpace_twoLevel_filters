5cfe871d2ccfe7ffab5055809831f10b0e9a7f86
jazzcatt <jazzcatt07@gmail.com>
2016-11-02 14:44:58 +0200

    initialization head and side menu from structure of data

--- src/js/main.js
+++ src/js/main.js
@@ -15,23 +15,34 @@ class App extends React.Component {
 	constructor(){
 		super();
 		this.state = {
-			head_menu: [
-				'US', 'Japan', 'Korea'
-			],
+			structure: structure,
+			head_menu: [],
 			head_menu_state: {
-				last_chosen: 'US',
-				US: 'active'
-			}, //head_menu
+
+			}, 
 			side_menu: [],
 			side_menu_state: {} 
 		} //state
 	}
 	componentDidMount() {
-		let elems = structure[this.state.head_menu_state.last_chosen];
+		// elements initializations head menu
+		let head_elems = this.getHeadElems(this.state.structure);
 		this.setState({
-			side_menu: elems
-		});
+			head_menu: head_elems,
+			head_menu_state: {last_chosen: head_elems[0], [head_elems[0]]: 'active'}
+		},()=>{    //initialization side_menu
+			let elems = structure[this.state.head_menu_state.last_chosen];
+			this.setState({side_menu: elems})});
+		}
+
+	getHeadElems(struct) {
+		let head_elems = [];
+		for(let key in struct) {
+			head_elems.push(key);
+		}
+		return head_elems;
 	}
+
 	headClick(e) { 
 		if(e.target.id == this.state.head_menu_state.last_chosen)
 			return;  // chosen the same button
